7e5bf0c339751a1fe15bbffceb5c6fb685efa79e
Opilane TTHK <opilane@tthk.ee>
2025-10-17 13:33:15 +0300

    DoWhile näidis

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿string kasutajaNimi = "";
+do
+{
+    Console.WriteLine("Palun sisesta oma kasutajanimi: ");
+    kasutajaNimi = Console.ReadLine();
+} while (kasutajaNimi != "user1");
+if (kasutajaNimi == "user1")
+{
+    int ruuduSuurus = 0;
+
+    do
+    {
+        Console.WriteLine("Kui suurt ruutu saada taahad?");
+        ruuduSuurus = int.Parse(Console.ReadLine());
+    } while (ruuduSuurus < 0 || ruuduSuurus > 20);
+
+    char reaMujund = '█';
+    string üksRida = "";
+    int tsükliMuutuja = ruuduSuurus;
+
+
+    do
+    {
+        üksRida = üksRida+"_" + reaMujund;
+        tsükliMuutuja = tsükliMuutuja - 1; 
+    } while (tsükliMuutuja != 0);
+
+    tsükliMuutuja = ruuduSuurus;
+
+    do
+    {
+        Console.WriteLine(üksRida);
+        tsükliMuutuja -= 1;
+    } while (tsükliMuutuja != 0);
+
+    Console.WriteLine($"Palun, siion on sinu ruut, suurusega {ruuduSuurus}x{ruuduSuurus}");
+}
