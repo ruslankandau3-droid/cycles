6533ad6744dce78b38255aa5e091f59f224a557c
Opilane TTHK <opilane@tthk.ee>
2025-10-24 13:25:51 +0300

    While nädis koos vooskeemiga

---
+++ While/While/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("joonista sulle kolmnurga");
+int kolmnurgaSuurus = int.Parse(Console.ReadLine());
+
+int i = 1; 
+while (i <= kolmnurgaSuurus)
+{
+    Console.WriteLine();
+    int j = 1;
+    while (j <= i)
+    {
+        Console.Write("█▒");
+        j++;
+    }
+    i++;
+}
